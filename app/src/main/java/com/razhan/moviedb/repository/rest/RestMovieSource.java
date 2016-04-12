41b1d8e2c30be8bcc7bf11fc65a73558bf3d6ceb
Ran <ranzhang.su@gmail.com>
2016-04-12 10:41:50 +0800

    init

--- app/src/main/java/com/razhan/moviedb/repository/rest/RestMovieSource.java
+++ app/src/main/java/com/razhan/moviedb/repository/rest/RestMovieSource.java
@@ -11,6 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
